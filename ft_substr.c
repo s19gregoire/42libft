262aeb72b601317c57859f38fa6e02bfb976e589
gneve <gneve@student.s19.be>
2020-11-23 03:06:33 -0800

    gregoire

--- ft_substr.c
+++ ft_substr.c
@@ -6,14 +6,13 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 03:08:49 by gneve             #+#    #+#             */
-/*   Updated: 2020/11/22 04:00:02 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/23 02:59:44 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char
-	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 03:23:50 by gneve             #+#    #+#             */
-/*   Updated: 2020/11/22 03:57:17 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/23 03:02:16 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,38 @@
 
 #include "stddef.h";
 #include "unistd.h";
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
+void			*ft_memset(void *b, int c, size_t len);
+void			ft_bzero(void *s, size_t n);
 unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char const *s, int fd);
+int				ft_atoi(const char *str);
+void			ft_bzero(void *s, size_t n);
+int 			ft_isalpha(char c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				t_isdigit(int c);
+char			*ft_itoa(int n);
+void			*ft_calloc(size_t nmemb, size_t size);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t len);
+void 			*ft_memchr(const void *s, int c, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putendl_fd(char const *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+void			ft_putstr_fd(char const *s, int fd);
+char			**ft_split(char const *s, char c);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(char *src);
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+int				ft_strlen(char *str);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int 			ft_strncmp(char *s1, char *s2, unsigned int n);
+char			*ft_strnstr(const char *str, const char *to_find, size_t len);
+char 			*ft_strrchr(const char *s, int c);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+int 			ft_toupper(int c);
 #endif
