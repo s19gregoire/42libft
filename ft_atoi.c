50bd873f49b9d5c746496a41efc9b5e32cc8e8be
gneve <gneve@student.s19.be>
2020-11-24 05:43:48 -0800

    g

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 02:39:46 by gneve             #+#    #+#             */
-/*   Updated: 2020/11/24 05:37:14 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/24 05:43:38 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ int		ft_atoi(const char *str)
 	i = 0;
 	neg = 1;
 	num = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == '\r')
+	while (str && str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -36,7 +35,7 @@ int		ft_atoi(const char *str)
 			if (neg < 0)
 				return (0);
 			else
-				return (1);						
+				return (1);
 		}
 	}
 	return (num * neg);
