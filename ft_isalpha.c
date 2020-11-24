87d32613f52cd0180b32684ad1759ee66487ceb5
gneve <gneve@student.s19.be>
2020-11-24 05:24:20 -0800

    g

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,13 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 01:48:31 by gneve             #+#    #+#             */
-/*   Updated: 2020/11/22 04:00:06 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/24 05:24:10 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha(char c)
+#include "libft.h"
+
+int		ft_isalpha(char c)
 {
 	if ((c > 64 && c < 91) || (c > 96 && c < 123))
 		return (1);
--- ft_memchr.c
+++ ft_memchr.c
@@ -6,16 +6,16 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 01:10:51 by neve              #+#    #+#             */
-/*   Updated: 2020/11/23 03:36:12 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/24 05:22:44 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char 	*ptr;
-	unsigned char 	c2;
+	unsigned char	*ptr;
+	unsigned char	c2;
 	int				i;
 
 	i = 0;
