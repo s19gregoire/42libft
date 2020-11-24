2b927c2b014568b158e2214d69b5283d04ca8236
gneve <gneve@student.s19.be>
2020-11-24 04:26:32 -0800

    gregoire

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 04:40:10 by neve              #+#    #+#             */
-/*   Updated: 2020/11/24 04:22:04 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/24 04:26:04 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void * src, size_t n)
+void	*ft_memcpy(void *dest, const void * src, size_t n)
 {
 	unsigned char	*s;
 	unsigned char	*d;
