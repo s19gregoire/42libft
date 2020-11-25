27a241c9e886892c99eae995fca3e11903d422fc
gneve <gneve@student.s19.be>
2020-11-25 12:45:16 -0800

    g

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 01:06:13 by neve              #+#    #+#             */
-/*   Updated: 2020/11/24 10:11:13 by gneve            ###   ########.fr       */
+/*   Updated: 2020/11/25 12:43:16 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t				i;
 	unsigned char		*ptr;
 	const unsigned char	*ptr2;
 
 	ptr = (unsigned char*)dst;
 	ptr2 = (unsigned char*)src;
-	i = 0;
-	if (!src)
+	if (!src && !dst)
 		return (NULL);
-	if (ptr2 < ptr)
-		while (++i <= len)
-			ptr[len - i] = ptr2[len - i];
-	else
+	if (dst < src)
+		ft_memcpy(dst, src, len);
+	else if(dst > src)
 		while (len-- > 0)
-			*(ptr++) = *(ptr2++);
+			ptr[len] = ptr2[len];
 	return (dst);
 }
